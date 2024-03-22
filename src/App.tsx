ed8a9eb3bca6df74043e6ee5c2dc45b46fe3dfc8
Abby Waddell <wabby.404@gmail.com>
2024-03-22 15:07:15 -0400

    Screen management optimized

--- src/App.tsx
+++ src/App.tsx
@@ -4,31 +4,36 @@ import InputForm from './components/InputForm';
 import LetterLines from './components/LetterLines';
 import Man from './components/Man';
 
+// template literal example yay :)
+type CurrentScreenType = 'start' | 'playing' | 'won' | 'lost';
+
 function App() {
   const words: string[] = ['corn', 'corgi', 'coo', 'phone', 'book', 'nook']
-  const [ gameFinished, setGameFinished ] = useState(false);
   const [ word, setWord ] = useState('');
   const [ wrongLetters, setWrongLetters ] = useState<string[]>([]);
   const [ correctLetters, setCorrectLetters ] = useState<string[]>([]);
+  const [ currentScreen, setCurrentScreen ] = useState<CurrentScreenType>('start');
 
   const playHangman = () => {
     // pick a random word from the array of words
     const wordToSet = words[Math.floor(Math.random() * words.length)];
     setWord(wordToSet);
-    // console.log(wordToSet);
     setCorrectLetters(Array(wordToSet.length).fill(' '));
+    setCurrentScreen('playing');
   }
 
   const checkLetter = (input: string) => {
     const inputArray = Array.from(word);
 
-    // if letter is found, do this
+    // if current input letter is found in word(word state), do this
       // add letter to correct spot, need a win condition: if all spots are not empty?
+    let lettersInPosition = correctLetters.slice();
+
     if(inputArray.includes(input)){
       console.log(correctLetters);
       console.log('letter found in word');
       // when letter is found in word, place letters in corresponding spots in correctLetters
-      let lettersInPosition = correctLetters.slice();
+
       inputArray.forEach((letter, index) => {
         if(input === letter){
           lettersInPosition[index] = input;
@@ -37,7 +42,7 @@ function App() {
       setCorrectLetters(lettersInPosition);
       // need to loop over to add letters where blank spaces exsist
       console.log(lettersInPosition);
-    // if letter is wrong, do this
+    // if current input letter is not found in word to guess, do this
     } else {
       const oldWrongLetters = wrongLetters.slice();
       // Dont add a repeat of a letter to list
@@ -48,46 +53,75 @@ function App() {
       console.log('letter not found!')
     }
 
+    console.log(lettersInPosition.join(''));
 
+    if(lettersInPosition.join('') === word){
+      setCurrentScreen('won');
+    }
     // win condition needs to be here!
     // if each index of correct letters has a letter in it, game won
-    // display win message, grey out input box, display play again button
+    // display win message, display play again button
 
+    // if wrong letters is 5 long, set game to lost
+    if(wrongLetters.length === 5){
+      setCurrentScreen('lost');
+    }
   }
 
-  // const renderSwitch(word: string, gameFinished: boolean) => {
-  //   switch(word, gameFinished) {
-  //     case (word && !gameFinished):
-  //       return (
-  //         <div>
-  //           <Man wrongLetters={wrongLetters}/>
-  //           <LetterLines word={word} correctLetters={correctLetters}/>
-  //           <InputForm checkLetter={checkLetter}/>
-  //         </div>
-  //       )
-  //     default:
-  //       return <PlayButton playHangman={playHangman}/>;
-  //   }
-  // };
+  const resetGame = () => {
+    setCurrentScreen('start');
+    setWord('');
+    setCorrectLetters([]);
+    setWrongLetters([]);
+  }
+
+
+  // make these pages their own file?
+  const renderSwitch = () => {
+    switch(currentScreen) {
+      case ('playing'):
+        return (
+          <div>
+            <Man wrongLetters={wrongLetters}/>
+            <LetterLines word={word} correctLetters={correctLetters}/>
+            <InputForm checkLetter={checkLetter}/>
+            {wrongLetters.map((letter,index) => {
+              return (
+                <div key={index}>{letter}</div>
+              )
+            })}
+          </div>
+        )
+
+      case('won'):
+          return (
+            <div>
+              <Man wrongLetters={wrongLetters}/>
+              <h3>You won! The word was {word}.</h3>
+              <h4>Play again?</h4>
+              {/* clear word, wrong letters, correctLetters IN THIS FN (so move it!) */}
+              <button onClick={() => resetGame()}>Play</button>
+            </div>
+          )
+
+      case('lost'):
+          return (
+            <div>
+              <Man wrongLetters={wrongLetters}/>
+              <h3>You lost! The word was {word}.</h3>
+              <h4>Play again?</h4>
+              <button onClick={() => resetGame()}>Play</button>
+            </div>
+          )
+      default:
+        return <PlayButton playHangman={playHangman}/>;
+    }
+  };
 
 
   return (
     <div>
-    {word ? 
-      <div>
-        <Man wrongLetters={wrongLetters}/>
-        <LetterLines word={word} correctLetters={correctLetters}/>
-        <InputForm checkLetter={checkLetter}/>
-      </div>
-
-      :
-      <PlayButton playHangman={playHangman}/>
-    }
-    {wrongLetters.map((letter,index) => {
-      return (
-        <div key={index}>{letter}</div>
-      )
-    })}
+      {renderSwitch()}
     </div>
   );
 }
